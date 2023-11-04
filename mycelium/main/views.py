132133c86b17d24f5df68b899da9ad0d27bccd7b
mewmewmew444 <marianekaterina2014@gmail.com>
2023-11-04 21:44:16 +1000

    ne poluchaetcaaaaaaaaaaaaaa

---
+++ mycelium/main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def index(request):
+    return render(request, 'main/index.html')
+
+    
--- mycelium/mycelium/settings.py
+++ mycelium/mycelium/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!hxd4+57a@s$oznjqj06mihowm)7(&6vxk8wmhnz74s1j$&(z&'
+SECRET_KEY = 'django-insecure--p!136e@m)tb-#8o^i6o!78+a7k6i1o3i916hqm*)f3v#)4#sj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
